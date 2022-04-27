47e5d465a3b097543a56986c7379aa1648ef6b40
Ambujbhaskar <bhaskarambuj7@gmail.com>
2022-04-27 23:54:53 +0530

    added setInterval

--- client/src/TextContainer.js
+++ client/src/TextContainer.js
@@ -2,15 +2,31 @@ import "./TextContainer.css";
 
 export default function TextContainer({textData}){
     const date = new Date();
-    return(
-        <>
-        <div className="textContainer">
-            <div className="senderName">{textData.Name}</div>
-            <div className="textBody">
-                {textData.Message_Body}
+    if(textData.isPicture=="0"){
+        return(
+            <>
+            <div className="textContainer">
+                <div className="senderName">{textData.Name}</div>
+                <div className="textBody">
+                    {textData.Message_Body}
+                </div>
+                <div className="date">{"Sent: "+textData.dateTime.toDateString() + ' ' + textData.dateTime.toLocaleTimeString()}</div>    
             </div>
-            <div className="date">{textData.dateTime.toDateString() + ' ' + textData.dateTime.toLocaleTimeString()}</div>    
-        </div>
-        </>
-    );
+            </>
+        );
+    }
+    else{
+        return(
+            <>
+            <div className="textContainer">
+                <div className="senderName">{textData.Name}</div>
+                <div className="textBody">
+                    <img src={textData.Message_Body} height="100px"/>
+                </div>
+                <div className="date">{"Sent: "+ textData.dateTime.toDateString() + ' ' + textData.dateTime.toLocaleTimeString()}</div>    
+            </div>
+            </>
+        );
+    }
+
 }
